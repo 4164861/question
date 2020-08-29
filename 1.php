df3cbb0d4cebc6f92c50658cf8f9739ceb3b7b35
4164861 <31242987+4164861@users.noreply.github.com>
2020-08-29 17:31:34 +0800

    first

--- 1.php
+++ 1.php
@@ -1,15 +1,3 @@
-<?php
-$xml = <<<EOD
-<?xml version="1.0" ?>
-<root xmlns:xi="http://www.w3.org/2001/XInclude">
- <xi:include href="file:///etc/passwd" parse="text"/>
-</root>
-EOD;
-$dom = new DOMDocument;
-// let's have a nice output
-$dom->preserveWhiteSpace = false;
-$dom->formatOutput = true;
-$dom->loadXML($xml);
-$dom->xinclude();
-echo $dom->saveXML();
+<?php 
+file_put_contents("test.txt", $_GET['file']) ; 
 ?>
